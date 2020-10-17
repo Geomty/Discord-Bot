11746bf9a803a0b56348fcac51ff0d7a7b4dc09e
Geomty <69478549+Geomty@users.noreply.github.com>
2020-10-17 11:06:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!';
+const prefix = '!'; // change the prefix here
+
 client.once('ready', () => {
     console.log(`The bot is online!`);
 });
+
 client.on('message', message => {
+    // these three lines below just help filter which messages are actually commands, don't worry about them
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
+    // this is the ping command
     if (command === 'ping') {
         message.channel.send('pong');
     }
 });
+
 client.login('(token)');
 // please put the token in the line above, where it says "(token")
