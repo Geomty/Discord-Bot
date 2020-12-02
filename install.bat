56eeaea3ace6a6a9deed012dcf679f981e27b15a
Geomty <69478549+Geomty@users.noreply.github.com>
2020-12-02 16:27:58 -0500

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+TITLE Installing discord.js...
+ECHO Please follow the steps below to install discord.js. If you want to skip a step because what's already provided is correct, just hit enter. If you don't know what to put for a step, you can skip it as well.
+PAUSE
+npm init
+ECHO Cool! You're almost done! The actual discord.js installation process will begin now. This shouldn't take long.
+PAUSE
+npm install discord.js
+ECHO You're all set to go! This window will now close. Just a reminder to delete this file or not open it again.
+PAUSE
